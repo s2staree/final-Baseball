c1155f1ffd9742b49052c0aabf4850beaa9b7ffd
s2staree <s2staree@gmail.com>
2022-09-23 17:02:54 +0900

    22-09-23 17:00 일단 저장!

--- src/main/java/site/metacoding/red/domain/stadium/StadiumDAO.java
+++ src/main/java/site/metacoding/red/domain/stadium/StadiumDAO.java
@@ -1,6 +1,11 @@
 package site.metacoding.red.domain.stadium;
 
+import java.util.List;
+
 public interface StadiumDAO {
 	public void insert(Stadium stadium);
-	public Stadium findById(Integer id);
+	public List<Stadium> findAll();
+	public void deleteById(int id);
+	public Stadium findByIdAndName(int id, String name);
+	public void update(Stadium stadium);
 }
--- src/main/java/site/metacoding/red/web/TestController.java
+++ src/main/java/site/metacoding/red/web/TestController.java
@@ -1,16 +1,22 @@
 package site.metacoding.red.web;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.red.domain.stadium.Stadium;
 import site.metacoding.red.domain.stadium.StadiumDAO;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class TestController {
 
 	private final StadiumDAO stadiumDAO;
@@ -21,20 +27,47 @@ public class TestController {
 	}
 	
 	@GetMapping("/stadium")
-	public String stadiumTest() {
+	public String stadiumTest(Model model) {
+		model.addAttribute("stadiums", stadiumDAO.findAll());
 		return "stadium/stadium";
+	}
+	
+	@GetMapping("test/stadium")
+	public @ResponseBody List<Stadium> stadiumTest() {
+		return stadiumDAO.findAll();
 	}
 
 	@PostMapping("/stadium")
 	public @ResponseBody String stadiumInsertProcTest(Stadium stadium) {
-		System.out.println("여긴 오나");
-		System.out.println(stadium);
+		System.out.println("@PostMapping" + stadium);
 		stadiumDAO.insert(stadium);
+		return "완료";
+	}
+	
+	@DeleteMapping("/stadium")
+	public @ResponseBody String stadiumDeleteProcTest(int id) {
+		System.out.println("@DeleteMapping" + id);
+		stadiumDAO.deleteById(id);
+		return "완료";
+	}
+
+	@PutMapping("/stadium")
+	public @ResponseBody String stadiumUpdateProcTest(Stadium stadium) {
+		System.out.println("@PutMapping" + stadium);
+		stadiumDAO.update(stadium);
 		return "완료";
 	}
 
 	@GetMapping("/stadiumInsertForm")
 	public String stadiumInsertFormTest() {
 		return "stadium/stadiumInsertForm";
+	}
+	
+	@GetMapping("/stadiumUpdateForm/{id}/{name}")
+	public String stadiumUpdateFormTest(@PathVariable int id,@PathVariable String name,Model model) {
+		System.out.println(id+","+name);
+		Stadium stadium = stadiumDAO.findByIdAndName(id, name);
+		model.addAttribute("stadium", stadium);
+		return "stadium/stadiumUpdateForm";
 	}
 }
